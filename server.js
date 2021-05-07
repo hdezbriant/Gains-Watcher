00ecda67b4332f818bbb5278facc49c08dbdf4f2
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-07 00:57:12 -0700

    configured connection

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutdb', {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true
 });
-
-// routes
-app.use(require('./public/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
