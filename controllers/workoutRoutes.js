055781a41d07bce49409730a58ce7d3a103d58fa
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-08 04:04:29 -0700

    completed create workout route

--- controllers/workoutRoutes.js
+++ controllers/workoutRoutes.js
@@ -20,11 +20,19 @@ router.put("/workouts/:id", async (req, res) => {
     );
     res.status(200).json(continuingWorkout);
   } catch (err) {
-    console.log(err);
     res.status(500);
   }
 });
 
+router.post("/workouts", async (req, res) => {
+  try {
+      const newWorkout = await db.Workout.create(req.body);
+      console.log(newWorkout);
+      res.status(200).json(newWorkout);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
 
 router.get("/workouts/range", async (req, res) => {
   try {
