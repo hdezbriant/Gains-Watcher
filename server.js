5a514fd19e5d5d35bcf556cfb60a5379da6ebeab
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-07 22:56:50 -0700

    initial deployment on heroku with atlas

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const routes = require('./controllers');
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
 });
 
 app.listen(PORT, () => {
