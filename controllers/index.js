5ed75a66f591d2b425502cfc4473dabd25c002b7
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-08 03:14:31 -0700

    uncommented api routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-// const apiRoutes = require("./workoutRoutes");
+const workoutRoutes = require("./workoutRoutes");
 const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
-// router.use("/api", apiRoutes);
+router.use("/api", workoutRoutes);
 
 module.exports = router;
